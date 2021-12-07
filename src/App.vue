36e521acef3db773503246311b7a161f849d4cfb
ArvinZJC <tomzjc@qq.com>
2021-12-07 23:18:43 +0000

    feat: adjust dependencies
    1. Add Syncfusion Vue UI Components (Essential JS 2).
    2. Drop TailwindCSS.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,64 @@
 <!--
  * @Description: the root component
- * @Version: 1.0.0.20211206
+ * @Version: 1.0.0.20211207
  * @Author: Arvin Zhao
  * @Date: 2021-12-06 21:52:09
  * @Last Editors: Arvin Zhao
- * @LastEditTime: 2021-12-06 23:05:50
+ * @LastEditTime: 2021-12-07 23:03:02
 -->
 
 <template>
-  <div id="app" class="flex p-5">Test</div>
+  <ejs-grid :dataSource="data">
+    <e-columns>
+      <e-column
+        field="OrderID"
+        headerText="Order ID"
+        textAlign="Right"
+        :isPrimaryKey="true"
+        width="100"
+      ></e-column>
+      <e-column field="CustomerID" headerText="Customer ID" width="80"></e-column>
+      <e-column field="ShipCountry" headerText="Ship Country" width="90"></e-column>
+    </e-columns>
+  </ejs-grid>
 </template>
 
 <script>
-export default {
-  name: "App"
-}
-</script>
+import { GridComponent, ColumnsDirective, ColumnDirective } from "@syncfusion/ej2-vue-grids";
+import "../node_modules/@syncfusion/ej2-base/styles/tailwind.css";
+import "../node_modules/@syncfusion/ej2-buttons/styles/tailwind.css";
+import "../node_modules/@syncfusion/ej2-calendars/styles/tailwind.css";
+import "../node_modules/@syncfusion/ej2-dropdowns/styles/tailwind.css";
+import "../node_modules/@syncfusion/ej2-inputs/styles/tailwind.css";
+import "../node_modules/@syncfusion/ej2-navigations/styles/tailwind.css";
+import "../node_modules/@syncfusion/ej2-popups/styles/tailwind.css";
+import "../node_modules/@syncfusion/ej2-splitbuttons/styles/tailwind.css";
+import "../node_modules/@syncfusion/ej2-vue-grids/styles/tailwind.css";
 
-<style>
-body, html {
-  @apply bg-white;
-}
-</style>
+export default {
+  name: "App",
+  // Declaring component and its directives
+  components: {
+    "ejs-grid": GridComponent,
+    "e-columns": ColumnsDirective,
+    "e-column": ColumnDirective,
+  },
+  // Bound properties declarations
+  data() {
+    return {
+      data: [
+        {
+          OrderID: 10248,
+          CustomerID: "VINET",
+          ShipCountry: "France",
+        },
+        {
+          OrderID: 10249,
+          CustomerID: "TOMSP",
+          ShipCountry: "Germany",
+        },
+      ],
+    };
+  },
+};
+</script>
