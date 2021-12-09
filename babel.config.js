cc795b3808abcef2c1367b7405f236578ced2385
ArvinZJC <tomzjc@qq.com>
2021-12-09 10:37:53 +0000

    build: adjust project configurations

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,12 @@
 /*
  * @Description: Babel configuration
- * @Version: 1.0.0.20211206
+ * @Version: 1.0.0.20211209
  * @Author: Arvin Zhao
  * @Date: 2021-12-06 21:52:09
  * @Last Editors: Arvin Zhao
- * @LastEditTime: 2021-12-06 22:53:25
+ * @LastEditTime: 2021-12-09 01:13:14
  */
 
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
+  presets: ["@vue/app"],
 };
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+/*
+ * @Description: Vue configuration
+ * @Version: 1.0.0.20211209
+ * @Author: Arvin Zhao
+ * @Date: 2021-12-09 00:57:09
+ * @Last Editors: Arvin Zhao
+ * @LastEditTime: 2021-12-09 00:57:09
+ */
+
+module.exports = {
+  configureWebpack: {
+    devtool: "source-map",
+  },
+};
