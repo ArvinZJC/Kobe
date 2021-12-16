218cead92532d3b72186e430f2667ec81f5e743b
ArvinZJC <tomzjc@qq.com>
2021-12-16 21:08:42 +0000

    feat: add the stock list data retriever and updater

---
+++ extensions/StockList/README-en.md
@@ -0,0 +1,16 @@
+# [Kobe](../../../..)/StockList
+
+[简体中文](./README-zhCN.md) | **English**
+
+TODO: This folder contains a Jupyter Notebook file with Python code to retrieve and store a list of symbols and corresponding names of SH/SZ stocks in a JSON file which is a part of the resources of the app. The JSON file is processed to provide the autocomplete functionality of the text box control for inputing the symbol of an SH/SZ stock.
+
+## ATTENTION
+
+1. The IPYNB file can be opened and edited in Visual Studio Code (if supported). By 16 December 2021, everything looks good with the code editor (Version: 1.63.2). In addition, I would like to thank [TuShare Pro](https://tushare.pro/) for its valuabale data. The primary packages used in the development are listed in the following table.
+
+   |     Name      | Version |
+   | :-----------: | :-----: |
+   |    python     | 3.10.1  |
+   |    pandas     |  1.3.4  |
+   | python-dotenv | 0.19.2  |
+   |    tushare    | 1.2.77  |
---
+++ extensions/StockList/retriever.py
@@ -0,0 +1,40 @@
+"""
+Description: a stock list raw data retriever
+Version: 1.0.0.20211216
+Author: Arvin Zhao
+Date: 2021-12-16 19:54:01
+Last Editors: Arvin Zhao
+LastEditTime: 2021-12-16 20:30:14
+"""
+
+import os
+
+from dotenv import load_dotenv
+import pandas as pd
+import tushare as ts
+
+
+def retrieve() -> pd.DataFrame | None:
+    """Retrieve the stock list raw data.
+
+    Returns
+    -------
+    DataFrame / None
+        The stock list raw data. Return null if Tushare Pro API token is not provided.
+    """
+    load_dotenv()
+    token = os.getenv("TUSHARE_PRO_TOKEN")  # Tushare Pro API token.
+
+    if token is None:
+        print("Failed to retrieve the stock list raw data.")
+        return None
+
+    ts_pro = ts.pro_api(token=token)  # Initialise Tushare Pro API with the token.
+    return ts_pro.stock_basic(
+        exchange="", list_status="L", fields="ts_code, name"
+    )  # Use the specified API to get the specified data object.
+
+
+# For simple tests only.
+if __name__ == "__main__":
+    retrieve()
