f7df8c51a25c5e748827126a645bf608303f131f
ArvinZJC <tomzjc@qq.com>
2022-02-26 21:17:25 +0800

    fix: the builder generates wrong info for updates

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 /*
  * @Description: Vue configuration
- * @Version: 1.0.3.20220225
+ * @Version: 1.0.5.20220226
  * @Author: Arvin Zhao
  * @Date: 2021-12-09 00:57:09
  * @Last Editors: Arvin Zhao
- * @LastEditTime: 2022-02-25 23:06:59
+ * @LastEditTime: 2022-02-26 21:04:09
  */
 
 module.exports = {
@@ -15,19 +15,19 @@ module.exports = {
     electronBuilder: {
       builderOptions: {
         appId: "com.isarvin.kobe",
+        artifactName: "${name}_${version}_${os}_${arch}.${ext}",
         generateUpdatesFilesForAllChannels: true,
         mac: {
           category: "public.app-category.utilities",
           target: [
             {
               arch: ["arm64", "x64"],
-              target: "dmg",
+              target: "default", // Squirrel.Mac requires the zip target. Reference: https://www.electron.build/auto-update#quick-setup-guide
             },
           ],
         },
         nsis: {
           allowToChangeInstallationDirectory: true,
-          artifactName: "${productName}-${version}-${arch}.${ext}",
           createDesktopShortcut: "always",
           installerLanguages: "zh_CN",
           language: "2052",
