c143f701f8db5c594c88f8d8df90a19b1e8fa1a9
ArvinZJC <tomzjc@qq.com>
2022-04-01 09:11:21 +0800

    feat: improve console messages

--- extensions/StockListUpdateChecker/src/retriever.py
+++ extensions/StockListUpdateChecker/src/retriever.py
@@ -1,11 +1,11 @@
 """
 '''
 Description: a stock list raw data retriever
-Version: 1.0.2.20220329
+Version: 1.0.3.20220401
 Author: Arvin Zhao
 Date: 2021-12-16 19:54:01
 Last Editors: Arvin Zhao
-LastEditTime: 2022-03-29 19:06:16
+LastEditTime: 2022-04-01 09:09:08
 '''
 """
 
@@ -22,16 +22,18 @@ def retrieve() -> Optional[pd.DataFrame]:
     Returns
     -------
     DataFrame / None
-        The stock list raw data. Return null if Tushare Pro API token is not provided.
+        The stock list raw data. Return null if the Tushare Pro API token is not provided.
     """
 
-    token = os.getenv("TUSHARE_PRO_TOKEN")  # Tushare Pro API token.
+    tushare_token = os.getenv("TUSHARE_PRO_TOKEN")
 
-    if token is None:
-        print("Failed to retrieve the stock list raw data.")
+    if tushare_token is None:
+        print("No Tushare Pro API token found.")
         return None
 
-    ts_pro = ts.pro_api(token=token)  # Initialise Tushare Pro API with the token.
+    ts_pro = ts.pro_api(
+        token=tushare_token
+    )  # Initialise Tushare Pro API with the token.
     return ts_pro.stock_basic(
         fields="ts_code, name"
     )  # Use the specified API to get the specified data object.
